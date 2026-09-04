b5aa96310ef6bd667d105671d2b783bb7e39f6a0
Krishna206204 <krishnabhatta526@gmail.com>
2026-09-04 11:34:09 +0545

    login, logout and dashboard for the staff

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Book,book_category
+from .models import Book,book_category,stock
 admin.site.register(book_category)
 admin.site.register(Book)
-
+admin.site.register(stock)
