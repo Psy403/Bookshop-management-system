200cbbf93a036f4d4610cb99487d1f8eaa7bd4d0
Krishna206204 <krishnabhatta526@gmail.com>
2026-09-05 17:49:14 +0545

    added url

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login/", views.staff_login, name="staff_login"),
+    path("", views.staff_login, name="staff_login"),
     path("logout/", views.staff_logout, name="staff_logout"),
     path("dashboard/", views.dashboard, name="dashboard"),
 ]
